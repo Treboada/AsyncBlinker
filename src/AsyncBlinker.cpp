45af8ba516128d07ee3c2b2c2d702649fbe40c10
Rafa Couto <caligari@treboada.net>
2017-09-17 16:26:19 +0200

    AsyncBlinker class and implementation.

---
+++ src/AsyncBlinker.cpp
@@ -0,0 +1,78 @@
+/** AsyncBlinker.cpp
+ *
+ * AsyncBlinker implementation.
+ *
+ * @version 1.0.0
+ * @author Rafa Couto <caligari@treboada.net>
+ * @license GNU General Public License v3.0
+ * @see https://github.com/Treboada/AsyncBlinker
+ *
+ */
+
+#include "AsyncBlinker.h"
+
+
+// blinks each second (500ms ON and 500ms OFF):
+static const uint16_t _default_blink[2] = { 500, 500 };
+
+
+AsyncBlinker::AsyncBlinker(Callback cb)
+{
+    _callback = cb;
+    setIntervals(_default_blink, sizeof(_default_blink) / 2);
+}
+
+
+
+void AsyncBlinker::setIntervals(const uint16_t* intervals, uint8_t count)
+{
+    _intervals = intervals;
+    _count = count;
+    _cycles = 0;
+}
+
+
+
+void AsyncBlinker::start(uint8_t cycles, uint8_t from, uint8_t to)
+{
+    _cycles = cycles;
+    _from = from;
+    _to = to;
+
+    _current = _from;
+    _past_millis = 0;
+
+    if (_callback && cycles > 0 && _intervals[from] > 0) _callback(true);
+}
+
+
+
+void AsyncBlinker::stop()
+{
+    _cycles = 0;
+}
+
+
+
+bool AsyncBlinker::tickUpdate(uint32_t elapsed_millis)
+{
+    if (_cycles == 0) return false;
+
+    _past_millis += elapsed_millis;
+    while (_past_millis >= _intervals[_current]) {
+
+        _past_millis -= _intervals[_current];
+
+        if (++_current > _to) {
+            _current = _from;
+            if (_cycles != ENDLESSLY && --_cycles <= 0) return false;
+        }
+
+        if (_callback && (_intervals[_current] > 0)) {
+            _callback(_current % 2 == 0);
+        }
+    }
+
+    return true;
+}
+
---
+++ src/AsyncBlinker.h
@@ -0,0 +1,60 @@
+/** AsyncBlinker.h
+ *
+ * AsyncBlinker class.
+ *
+ * @version 1.0.0
+ * @author Rafa Couto <caligari@treboada.net>
+ * @license GNU General Public License v3.0
+ * @see https://github.com/Treboada/AsyncBlinker
+ *
+ */
+
+#ifndef _ASYNC_BLINKER_H
+#define _ASYNC_BLINKER_H
+
+#include <stdint.h>
+
+/**
+ * This class manages a blinking led (or any switchable device) changing the
+ * light in ON-OFF time intervals.
+ *
+ */
+class AsyncBlinker
+{
+    public:
+
+        static const int ENDLESSLY = UINT8_MAX;
+
+        typedef void (*Callback)(bool enable);
+
+        AsyncBlinker(Callback cb);
+
+        void setIntervals(const uint16_t* intervals, uint8_t count);
+
+        void setCallback(Callback cb) { _callback = cb; }
+
+        void start(uint8_t cycles, uint8_t from, uint8_t to);
+        void start(uint8_t cycles) { start(cycles, 0, _count - 1); }
+        void start() { start(ENDLESSLY); }
+
+        void stop();
+
+        bool tickUpdate(uint32_t elapsed_millis);
+
+    private:
+
+        const uint16_t* _intervals;
+        uint8_t _count;
+
+        Callback _callback;
+
+        uint8_t _from;
+        uint8_t _to;
+        uint8_t _current;
+
+        uint32_t _past_millis;
+        uint8_t _cycles;
+};
+
+#endif // _ASYNC_BLINKER_H
+
